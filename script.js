345cffa051fb0b4acce4005ecfaec0a67ddf8e7e
nihalDevidas <117895185+nihalDevidas@users.noreply.github.com>
2023-12-11 23:39:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+
+function getData(urls){
+	const request = new XMLHttpRequest();
+	for(let url of urls){
+
+		 request.open('GET',`${url}`);
+         request.send();
+        
+        // to get the response that is sent
+        request.addEventListener('load',function(){
+         const data = JSON.parse(this.responseText);
+			console.log(data)
+		})
+	}
+         
+}
+
+const urls = [
+  'https://jsonplaceholder.typicode.com/posts/1',
+  'https://jsonplaceholder.typicode.com/posts/2',
+  'https://jsonplaceholder.typicode.com/posts/3'
+];
+getData(urls);
